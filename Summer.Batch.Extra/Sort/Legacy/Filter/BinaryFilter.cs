8e35191cadb69f52c82e4213724fe92a0b52e988
rahulsaraf <rahulsaraf1489@gmail.com>
2016-02-16 15:53:20 -0600

    Binary - Hex criteria support - SORT functionality

---
+++ Summer.Batch.Extra/Sort/Legacy/Filter/BinaryFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summer.Batch.Extra.Sort.Legacy.Filter
+{
+    class BinaryFilter: AbstractLegacyFilter<decimal>
+    {
+        protected override int DoComparison(decimal leftValue, decimal rightValue)
+        {
+            return leftValue.CompareTo(rightValue);
+        }
+    }
+}
