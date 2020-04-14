457d13dfab4d4d0cac710d5b46a2ca5485f0c9c7
doshinam <doshinameeta@gmail.com>
2020-04-14 14:27:12 -0500

    updated assembly info to v1.2 and added Release Notes

--- Summer.Batch.Data/Properties/AssemblyInfo.cs
+++ Summer.Batch.Data/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Summer.Batch.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -45,5 +45,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.13")]
-[assembly: AssemblyFileVersion("1.1.13")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
